03f0087b5db8bb34ed6df7ffd74d0c14a8b9a30e
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-05-17 12:44:13 +0530

    update decorators.py

--- helpers/decorators.py
+++ helpers/decorators.py
@@ -7,6 +7,18 @@ from helpers.admins import get_administrators
 from config import SUDO_USERS
 
 
+@Client.on_message(filters.command(["auth", "auth@OhtoAiPlaysBot"]) & other_filters) 
+@errors 
+@authorized_users_only
+async def auth(client: Client, message: Message):
+  user = await message.from_user
+  if user.id in SUDO_USERS:
+    await message.reply(f"{user.first_name} is already authorized")
+    return
+  SUDO_USERS.append(user.id)
+  await message.reply(f"{user.first_name} was authorized to use my commands")
+  
+
 def errors(func: Callable) -> Callable:
     async def decorator(client: Client, message: Message):
         try:
