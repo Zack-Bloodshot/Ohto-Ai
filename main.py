98814671d62d45f326888f0075b14f5591f7b8ce
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-05-17 12:46:41 +0530

    update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pyrogram import Client as Bot
-
+import logging 
 from callsmusic import run
 from config import API_ID, API_HASH, BOT_TOKEN
 
@@ -11,5 +11,7 @@ bot = Bot(
     plugins=dict(root="handlers")
 )
 
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level = logging.INFO)
+ 
 bot.start()
 run()
