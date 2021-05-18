894d0df025684ef4564368f5c4ad8b3072b67849
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-05-18 16:43:10 +0530

    update admins.py

--- handlers/admins.py
+++ handlers/admins.py
@@ -10,62 +10,6 @@ from helpers.filters import command, other_filters
 from helpers.decorators import errors, authorized_users_only
 from handlers.play import quu 
 
-auth = []
-
-@Client.on_message(filters.command(["auth", "auth@OhtoAiPlaysBot"]) & other_filters) 
-@errors 
-@authorized_users_only
-async def aauth(client: Client, message: Message):
-  user = message.reply_to_message.from_user
-  if not user:
-    return await message.reply("Reply to a user baka!")
-  if user.id in auth:
-    await message.reply(f"{user.first_name} is already authorized")
-    return
-  auth.append(user.id)
-  await client.send_message(-1001418899867, auth)
-  await message.reply(f"{user.first_name} was authorized to use my commands")
- 
-@Client.on_message(filters.command(["remauth", "remauth@OhtoAiPlaysBot"]) & other_filters) 
-@errors 
-@authorized_users_only
-async def rauth(client: Client, message: Message):
-  reply = message.reply_to_message
-  if not reply:
-    return await message.reply("Reply to a user baka!")
-  user = reply.from_user
-  if not user.id in auth:
-    await message.reply(f"[{user.first_name}](tg://user?id={user.id}) is already unauthorized", parse_mode = "md")
-    return
-  auth.remove(user.id)
-  await client.send_message(-1001418899867, auth)
-  await message.reply(f"[{user.first_name}](tg://user?id={user.id}) was unauthorized to use my commands", parse_mode = "md")
- 
-  
-@Client.on_message(filters.command(["listauth", "listauth@OhtoAiPlaysBot"]) & other_filters) 
-@errors 
-@authorized_users_only
-async def lauth(client: Client, message: Message):
-  text = ""
-  count = 0
-  for user in auth:
-    count += 1
-    text += count 
-    text += f"`{user}`\n"
-  await message.reply(text)
- 
-@Client.on_message(filters.command(["reload", "reload@OhtoAiPlaysBot"]) & other_filters) 
-@errors
-@authorized_users_only 
-async def rload(client: Client, message: Message):
-  count = 0
-  async for msg in client.search(-1001418899867):
-    if count == 1:
-      break 
-    auth = msg.text
-    count = 1 
-  await message.reply("Reloaded Successfully ^_^!!! ")
-
 @Client.on_message(filters.command(["pause", "pause@OhtoAiPlaysBot"]) & other_filters)
 @errors
 @authorized_users_only
@@ -120,8 +64,12 @@ async def stop(_, message: Message):
 @errors
 @authorized_users_only
 async def skip(_, message: Message):
-    if message.chat.id not in callsmusic.pytgcalls.active_calls:
-        await message.reply_text("Ahh baka, Nuthin even playin..... ",parse_mode = "md")
+    if callsmusic.queues.is_empty(message.chat.id):
+      try:
+        callsmusic.pytgcalls.leave_group_call(message.chat.id) 
+      except Exception:
+        pass
+      await message.reply_text("Ahh baka, Nuthin even playin..... ",parse_mode = "md")
     else:
         callsmusic.queues.task_done(message.chat.id)
         try:
