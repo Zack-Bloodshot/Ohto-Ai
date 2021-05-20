5446e56fde61c2611b50150b6f73ef9a0feb1db1
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-05-20 12:17:56 +0530

    update __init__.py

--- sql/__init__.py
+++ sql/__init__.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 
 
 def start() -> scoped_session:
-    engine = create_engine(DB_URI, client_encoding="utf8")
+    engine = create_engine("sqldbtype://username:pw@hostname:port/db_name", client_encoding="utf8")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
