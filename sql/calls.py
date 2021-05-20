88c02d0bef6e8ae009625c6c43b351d44987f228
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-05-20 12:23:45 +0530

    update calls.py

--- sql/calls.py
+++ sql/calls.py
@@ -1,1 +1,54 @@
 #calls sql.. 
+import threading
+
+from SaitamaRobot.modules.sql import BASE, SESSION
+from sqlalchemy import Column, String
+
+
+class PyKall(BASE):
+    __tablename__ = "vconchats"
+    chat_id = Column(String(14), primary_key=True)
+
+    def __init__(self, chat_id):
+        self.chat_id = chat_id
+
+
+Pykall.__table__.create(checkfirst=True)
+
+INSERTION_LOCK = threading.RLock()
+
+
+def is_call(chat_id): 
+  try:
+    chat = SESSION.query(Pykall).get(str(chat_id))
+    if chat:
+      return True
+    else:
+      return False 
+  finally:
+    SESSION.close()
+
+def set_on(chat_id):
+  with INSERTION_LOCK:
+    vc_on = SESSION.query(Pykall).get(str(chat_id))
+    if not vc_on:
+      vc_on = Pykall(str(chat_id))
+    else:
+      pass
+    SESSION.add(lewdfalse)
+    SESSION.commit()
+
+def set_off(chat_id):
+  with INSERTION_LOCK:
+   vc_on = SESSION.query(Pykall).get(str(chat_id))
+   if vc_on:
+     SESSION.delete(vc_on)
+   SESSION.commit()
+
+def get_vc_on(chat_id):
+  try:
+    return SESSION.query(Pykall.chat_id).all()
+  finally:
+    SESSION.close()
+
+
