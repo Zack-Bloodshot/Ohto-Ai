1d7b2bd802e725dc73f7d9c2ae6d884a892019c9
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-05-21 11:04:38 +0530

    update __init__.py

--- sql/__init__.py
+++ sql/__init__.py
@@ -2,10 +2,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
+import psycopg3 
+import os
 
+DB_URI = os.environ('DATABASE_URL')
 
 def start() -> scoped_session:
-    engine = create_engine("sqldbtype://username:pw@hostname:port/db_name", client_encoding="utf8")
+    engine = create_engine(DB_URI, client_encoding="utf8")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
