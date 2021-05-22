dcef2860c0bde34c1a579850dda2afca78724cbc
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-05-22 19:13:49 +0530

    update auth.py

--- sql/auth.py
+++ sql/auth.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Integer, func, distinct
 
-from SaitamaRobot.modules.sql import BASE, SESSION
+from sql import BASE, SESSION
 
 
 class Auth(BASE):
