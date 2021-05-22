a10324a212394994dd55658365b3e9b550acc1ff
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-05-22 20:24:06 +0530

    update decorators.py

--- helpers/decorators.py
+++ helpers/decorators.py
@@ -24,7 +24,7 @@ def authorized_users_only(func: Callable) -> Callable:
     async def decorator(client: Client, message: Message):
         if message.from_user.id in SUDO_USERS:
             return await func(client, message)
-        administrators = get_administrators(message.chat)
+        administrators = await get_administrators(message.chat)
         for administrator in administrators:
             if administrator == message.from_user.id:
                 return await func(client, message)
