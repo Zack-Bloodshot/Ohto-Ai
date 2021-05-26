b0b47adf447b21740c5e80966da60703206ef6b7
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-05-26 14:48:49 +0530

    update config.py

--- config.py
+++ config.py
@@ -7,7 +7,9 @@ load_dotenv()
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME")
-
+OWNER_ID = getenv("OWNER_ID")
+START_PIC = getenv("START_PIC")
+PLAY_PIC = getenv("PLAY_PIC")
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
