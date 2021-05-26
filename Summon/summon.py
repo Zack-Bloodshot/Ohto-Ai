eaf30bb79c975bf2c3669c0949abfdba3421cecb
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-05-26 22:23:58 +0530

    update summon.py

--- Summon/summon.py
+++ Summon/summon.py
@@ -4,7 +4,7 @@ from config import SESSION_NAME, API_ID, API_HASH
 temp = Client(SESSION_NAME, API_ID, API_HASH)
 
 async def joinchatto(client: Client, chat_id):
-  client.join_chat(chat_id)
+  await client.join_chat(chat_id)
   return True
   
 async def temo(): 
