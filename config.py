cbb4cec781dce9abe51ed6ec46ceb9fb1e1dca39
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-05-26 15:15:01 +0530

    update config.py

--- config.py
+++ config.py
@@ -5,13 +5,22 @@ from dotenv import load_dotenv
 load_dotenv()
 
 SESSION_NAME = getenv("SESSION_NAME", "session")
+
 BOT_TOKEN = getenv("BOT_TOKEN")
+
 BOT_NAME = getenv("BOT_NAME")
-OWNER_ID = getenv("OWNER_ID")
+
+OWNER_ID = getenv(int("OWNER_ID"))
+
 START_PIC = getenv("START_PIC")
+
 PLAY_PIC = getenv("PLAY_PIC")
+
 API_ID = int(getenv("API_ID"))
+
 API_HASH = getenv("API_HASH")
+
+BOT_USERNAME = getenv("BOT_USERNAME")
 
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
 
