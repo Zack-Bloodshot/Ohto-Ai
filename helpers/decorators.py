5437609c17106e4f134d85fc0d7dfc5e8e96e58f
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-05-27 17:13:25 +0530

    update decorators.py

--- helpers/decorators.py
+++ helpers/decorators.py
@@ -35,7 +35,7 @@ def authorized_users_only2(func: Callable) -> Callable:
     async def decorator(client: Client, message: Message):
         if message.from_user.id in SUDO_USERS:
             return await func(client, message)
-        if sql.is_approved(message.chat.id, message.from_user.id):
+        elif sql.is_approved(message.chat.id, message.from_user.id):
             return await func(client, message)
         else: 
           administrators = await get_administrators(message.chat)
@@ -43,6 +43,6 @@ def authorized_users_only2(func: Callable) -> Callable:
               if admin == message.from_user.id:
                 return await func(client, message)
               else: 
-                return await message.reply("Sorry..! only admemes and authorized memes!")
+                return await message.reply(f"{admin} ≠ {message.from_user.id}\nSorry..! only admemes and authorized memes!")
 
     return decorator
