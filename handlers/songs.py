bd193302e373628b096364c7bc61518855c6a100
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-06-22 18:31:16 +0530

    update songs.py

--- handlers/songs.py
+++ handlers/songs.py
@@ -21,7 +21,11 @@ async def song(client, message: Message):
     okvai = query.capitalize()
     print(query.capitalize())
     m = await message.reply(f"🔍 Searching for {okvai}")
-    ydl_opts = {"format": "bestaudio"}
+    ydl_opts = {
+      "format": "bestaudio",
+      "geo-bypass": True,
+      "outtmpl": %(id)s.mp3",
+    }
     try:
         results = []
         count = 0
@@ -58,7 +62,7 @@ async def song(client, message: Message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f'🎶 **Title:** [{title[:35]}]({link})\n⏳ **Duration:** {duration}\n👀 **Views:** {views}'
+        rep = f'🎶 *Title:* [{title[:35]}]({link})\n⏳ *Duration:* {duration}\n👀 *Views:* {views}'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
