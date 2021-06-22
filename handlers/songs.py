0dec1cfb3083439d9e6d90e3684ef39b2872dcb4
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-06-22 19:53:39 +0530

    update songs.py

--- handlers/songs.py
+++ handlers/songs.py
@@ -63,7 +63,7 @@ async def song(client, message: Message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f'Title: [{title[:35]}]({link})\n Duration: {duration}}\n Views: {views}'
+        rep = f'Title: [{title[:35]}]({link})\n Duration: {duration}\n Views: {views}'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
