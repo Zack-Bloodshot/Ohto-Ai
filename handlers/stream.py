d3620a74d21d25f2fd712c3ee2a5de807d8bde6f
Zack-Bloodshot <zackbloodshot@gmail.com>
2021-08-29 15:41:00 +0530

    0.0.3

--- handlers/stream.py
+++ handlers/stream.py
@@ -14,6 +14,7 @@ from helpers.decorators import errors
 from helpers.decorators import authorized_users_only
 from helpers.decorators import authorized_users_only2
 from config import API_ID, API_HASH, BOT_TOKEN, PLAY_PIC, BOT_USERNAME, OWNER_ID, UBOT_ID
+import moviepy.editor as soundex
 
 @Client.on_message(filters.command(["stream", f"stream@{BOT_USERNAME}"]) & other_filters)
 @errors
@@ -25,6 +26,9 @@ async def stream_vid(client: Client, message: Message):
   if not (video.file_name.endswith('.mkv') or video.file_name.endswith('.mp4')):
     return await message.reply_text('Not a valid format...')
   dl = await message.reply_to_message.download()
+  audio_file_name = str(dl).split('.', 1)[0]
+  ext_s = soundex.VideoFileClip(dl)
+  sound_clip = converter.convert(ext_s.audio.write_audiofile(f'{audio_file_name}.mp3'))
   try:
       group_call = await mp.call(message.chat.id)
   except RuntimeError:
@@ -33,5 +37,6 @@ async def stream_vid(client: Client, message: Message):
       return await message.reply_text('Seems like my assistant is not in the chat!')
   except Exception as e:
       return await message.reply_text(f'{type(e).__name__}: {e}')
+  group_call.input_filename = sound_clip
   await group_call.set_video_capture(dl)
   block_chat.append(message.chat.id)
