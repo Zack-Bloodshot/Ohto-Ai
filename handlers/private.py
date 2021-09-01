cb01ac59ab52575a1a792d174e97a33826e3b27f
Ninematsuno <89542501+Ninematsuno@users.noreply.github.com>
2021-09-01 20:31:46 +0530

    Update private.py
    Hehe

--- handlers/private.py
+++ handlers/private.py
@@ -20,9 +20,9 @@ async def start(_, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "Deploy me", url="https://heroku.com/deploy?template=https://github.com/Zack-Bloodshot/Ohto-Ai"
+                        "Commands", url="https://telegra.ph/Commands-09-01"
                     )],[
-                    InlineKeyboardButton(text = "Creator", url = "https://t.me/DontKnowWhyRU"), InlineKeyboardButton(text = "Repo", url = "https://github.com/Zack-Bloodshot/Ohto-Ai")]
+                    InlineKeyboardButton(text = "Creator", url = "https://t.me/NINE_EVIL"), InlineKeyboardButton(text = "SUPPORT", url = "https://t.me/mikey_player_support")]
             ]
         )
     )
