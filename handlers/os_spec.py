a37c163ae4c9e44748469cb2620f1502161756ae
Zack-Bloodshot <zackbloodshot@gmail.com>
2021-09-03 10:29:20 +0530

    mistakes

--- handlers/os_spec.py
+++ handlers/os_spec.py
@@ -2,9 +2,10 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 import config
 import subprocess
+from helpers.filters import command, other_filters
 
 @Client.on_message(filters.command(["neofetch", f"neofetch@{config.BOT_USERNAME}"]) & other_filters)
-async def neo_fetcher(_, mmessage: Message):
+async def neo_fetcher(_, message: Message):
   if message.from_user.id != config.OWNER_ID:
     return 
   process = subprocess.Popen(
